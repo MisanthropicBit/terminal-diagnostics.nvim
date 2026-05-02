c63e950bb509fd3a905d9efba51a618301c69ba9
MisanthropicBit <proximity.dev@proton.me>
2026-05-02 14:03:22 +0200

    Update command options

--- plugin/terminal-diagnostics.lua
+++ plugin/terminal-diagnostics.lua
@@ -1,11 +1,47 @@
-vim.api.nvim_create_user_command("TermDiagVersion", function() end, { nargs = 0 })
+local open_command_complete_args
 
-vim.api.nvim_create_user_command("TermDiagQuickfix", function() end, { nargs = 0 })
+local function open_command_complete()
+    if not open_command_complete_args then
+        -- TODO: Move OpenType to separate file?
+        open_command_complete_args =
+            vim.tbl_values(require("terminal-diagnostics.api.open").OpenType)
+    end
 
-vim.api.nvim_create_user_command("TermDiagPrevious", function() end, { nargs = 0 })
+    return open_command_complete_args
+end
 
-vim.api.nvim_create_user_command("TermDiagNext", function() end, { nargs = 0 })
+vim.api.nvim_create_user_command("TermDiagVersion", function()
+    vim.print(require("terminal-diagnostics").version())
+end, { nargs = 0, desc = "Print the current version" })
 
-vim.api.nvim_create_user_command("TermDiagOpen", function() end, { nargs = "?" })
+vim.api.nvim_create_user_command("TermDiagQuickfix", function() end, {
+    nargs = 0,
+    range = true,
+    desc = "Open the last command output, or a selected range, in the quickfix buffer",
+})
 
-vim.api.nvim_create_user_command("TermDiagSelect", function() end, { nargs = 1 })
+vim.api.nvim_create_user_command(
+    "TermDiagPrevious",
+    function() end,
+    { nargs = 0, count = 1, desc = "Jump to the [count] previous error" }
+)
+
+vim.api.nvim_create_user_command(
+    "TermDiagNext",
+    function() end,
+    { nargs = 0, count = 1, desc = "Jump to the [count] next error" }
+)
+
+vim.api.nvim_create_user_command("TermDiagOpen", function() end, {
+    nargs = "?",
+    complete = open_command_complete,
+    desc = "Open the error under the cursor",
+})
+
+vim.api.nvim_create_user_command("TermDiagSelect", function() end, {
+    nargs = "?",
+    complete = function()
+        return { "inner", "outer" }
+    end,
+    desc = "Visually select the error under cursor",
+})
