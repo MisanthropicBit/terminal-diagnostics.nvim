ac7b7b735adfe57dd036289f387d54ac6b2a90ce
MisanthropicBit <proximity.dev@proton.me>
2026-08-14 20:55:39 +0200

    Improvements to project diagnostics

--- lua/terminal-diagnostics/diagnostics/init.lua
+++ lua/terminal-diagnostics/diagnostics/init.lua
@@ -1,8 +1,9 @@
 local diagnostics = {}
 
-local SequentialOutputProcessor = require("terminal-diagnostics.output_processors.sequential")
+local SequentialOutputProcessor =
+    require("terminal-diagnostics.output_processors.sequential")
 local ParallelOutputProcessor = require("terminal-diagnostics.output_processors.parallel")
-local notify = require("terminal-diagnostics.notify")
+-- local buffer_cache = require("terminal-diagnostics.buffer_cache.buffer_cache")
 
 local ns_id = vim.api.nvim_create_namespace("terminal-diagnostics.diagnostics")
 local processor
@@ -10,7 +11,7 @@ local processor
 -- TODO:
 ---@alias terminal-diagnostics.DiagnosticsFilter fun(): boolean
 
----@class terminal-diagnostics.QuickfixOptions
+---@class terminal-diagnostics.ListOptions
 ---@field open  boolean?
 ---@field focus boolean?
 ---@field title string?
@@ -18,13 +19,31 @@ local processor
 ---@class terminal-diagnostics.DiagnosticsCreateOptions
 ---@field terminal_diagnostics boolean?
 ---@field diagnostics          boolean?
----@field quickfix             (boolean | terminal-diagnostics.QuickfixOptions)?
----@field locationlist         (boolean | terminal-diagnostics.QuickfixOptions)?
----@field trouble              boolean?
+---@field quickfix             (boolean | terminal-diagnostics.ListOptions)?
+---@field locationlist         (boolean | terminal-diagnostics.ListOptions)?
+---@field trouble              (boolean | terminal-diagnostics.ListOptions)?
 ---@field parallel             boolean?
----@field notify               boolean?
 ---@field filter               terminal-diagnostics.DiagnosticsFilter?
 ---@field stable               boolean? If the buffer is stable and won't be modified
+
+--- Determine if a buffer is stable and probably won't be modified in
+--- the future so we can cache the parse results
+---@param buffer integer
+---@return boolean
+local function is_stable_buffer(buffer)
+    local bufname = vim.fn.bufname(buffer)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = buffer })
+
+    if buftype == "terminal" then
+        if vim.startswith(bufname, "term://") then
+            return false
+        end
+
+        return true
+    end
+
+    return false
+end
 
 ---@return integer
 function diagnostics.namespace_id()
@@ -42,9 +61,49 @@ function diagnostics.create(diagnostic)
         diagnostic,
         {
             namespace = ns_id,
-            user_data = { generated_by = "terminal-diagnostics.nvim" },
+            user_data = vim.tbl_extend(
+                "force",
+                { generated_by = "terminal-diagnostics.nvim" },
+                diagnostic.user_data or {}
+            ),
         }
     )
+end
+
+---@param project_diagnostics vim.Diagnostic[]
+local function set_project_diagnostics(project_diagnostics)
+    -- TODO: Handle command specs like jest where we need to create project
+    -- diagnostics that group parse results together (header + multiple error
+    -- lines). We either can:
+    --
+    -- 1. Label each parse result with its group and later re-group them
+    -- 2. Group header + error lines for command specs that set a flag
+    if #project_diagnostics > 0 then
+        local buffers = vim.api.nvim_list_bufs()
+
+        ---@type table<integer, vim.Diagnostic[]>
+        local diagnostics_per_buffer = vim.defaulttable(function()
+            return {}
+        end)
+
+        -- Find buffers whose paths correspond to a project diagnostic
+        for _, diagnostic in ipairs(project_diagnostics) do
+            for _, buf in ipairs(buffers) do
+                if vim.api.nvim_buf_get_name(buf) == diagnostic.user_data.path then
+                    table.insert(diagnostics_per_buffer[buf], diagnostic)
+                end
+            end
+        end
+
+        -- Set diagnostics for all buffers. Buffers for paths in project
+        -- diagnostics will previously have been added as 'unlisted' to
+        -- the buffer list via vim.uri_to_bufnr
+        for buffer, buffer_diagnostics in pairs(diagnostics_per_buffer) do
+            diagnostics.set(buffer, buffer_diagnostics, {})
+        end
+    end
+
+    return project_diagnostics
 end
 
 ---@param buffer integer
@@ -74,9 +133,14 @@ function diagnostics.create_for_buffer(buffer, options)
 
     -- Create terminal diagnostics from parse results
     if options.terminal_diagnostics then
-        local terminal_diagnostics = diagnostics.terminal_from_parse_results(result.parse_results)
+        local terminal_diagnostics =
+            diagnostics.from_terminal_parse_results(result.parse_results)
 
         diagnostics.set(buffer, terminal_diagnostics, {})
+
+        -- if options.stable or is_stable_buffer(buffer) then
+        --     buffer_cache.set(buffer, result.parse_results)
+        -- end
     end
 
     local project_diagnostics = {}
@@ -86,17 +150,8 @@ function diagnostics.create_for_buffer(buffer, options)
     if options.diagnostics or populate_list then
         project_diagnostics = diagnostics.from_parse_results(result.parse_results)
 
-        -- TODO: Need to resolve paths to absolute paths and load the buffers
-        -- for those files.
-        --
-        -- Alternatively, set diagnostics for the buffers that are already open
-        -- and cache the rest. When a new buffer is opened, check if the
-        -- filename matches, set diagnostics, and clear cache entry (what if the
-        -- buffer is unloaded and then loaded again?). This is probably better
-        -- since we might otherwise load an enormous amount of buffers
-
-        if #project_diagnostics > 0 then
-            diagnostics.set(0, project_diagnostics, {})
+        if options.diagnostics then
+            set_project_diagnostics(project_diagnostics)
         end
     end
 
@@ -110,7 +165,9 @@ function diagnostics.create_for_buffer(buffer, options)
             diagnostics.setqflist(project_diagnostics, {
                 open = true,
                 focus = false,
-                title = ("terminal-diagnostics.nvim (%s)"):format(vim.iter(sources):join(", ")),
+                title = ("terminal-diagnostics.nvim (%s)"):format(
+                    vim.iter(sources):join(", ")
+                ),
             })
         end
 
@@ -119,19 +176,15 @@ function diagnostics.create_for_buffer(buffer, options)
 
             vim.fn.setloclist(win_id, {}, " ", {})
         end
-    end
 
-    -- Notify the user
-    if options.notify then
-        notify.info("Processed terminal output")
-    end
+        if options.trouble then
+            local has_trouble, _ = pcall(require, "trouble")
 
-    -- Fire autocommand to signal completion of processor
-    vim.api.nvim_exec_autocmds("User", {
-        pattern = "terminal-diagnostics.processor.completed",
-        modeline = false,
-        data = { event = event },
-    })
+            if has_trouble then
+                vim.cmd("Trouble qflist open")
+            end
+        end
+    end
 end
 
 ---@param parse_results terminal-diagnostics.parser.ParseResult[]
@@ -141,23 +194,32 @@ function diagnostics.from_parse_results(parse_results)
 
     for _, parse_result in ipairs(parse_results) do
         local buffer = 0
+        local path = parse_result.values.paths[1]
+        local severity = parse_result.values.severity or "info"
 
         -- TODO: How to handle multiple valid paths?
-        if parse_result.values.paths[1] then
-            buffer = vim.uri_to_bufnr(vim.uri_from_fname(parse_result.values.paths[1]))
+        if path then
+            buffer = vim.uri_to_bufnr(vim.uri_from_fname(path))
+        end
+
+        local message = parse_result.values.message or "No message"
+
+        if parse_result.context and #parse_result.context.lines > 0 then
+            message = message .. "\n" .. table.concat(parse_result.context.lines, "\n")
         end
 
         local diagnostic = diagnostics.create({
             bufnr = buffer,
             lnum = parse_result.values.lnum - 1,
             col = parse_result.values.col - 1,
-            end_lnum = 0, -- TODO:
+            end_lnum = parse_result.values.lnum,
             end_col = 0, -- TODO:
-            severity = vim.diagnostic.severity[parse_result.values.severity],
-            message = parse_result.values.message,
-            source = parse_result.source,
+            severity = vim.diagnostic.severity[severity],
+            message = message,
+            source = parse_result.command_spec:name(),
             code = parse_result.values.code,
             valid = buffer ~= nil,
+            user_data = { path = path },
         })
 
         table.insert(_diagnostics, diagnostic)
@@ -168,7 +230,7 @@ end
 
 ---@param parse_results terminal-diagnostics.parser.ParseResult[]
 ---@return vim.Diagnostic[]
-function diagnostics.terminal_from_parse_results(parse_results)
+function diagnostics.from_terminal_parse_results(parse_results)
     local terminal_diagnostics = {}
 
     for _, parse_result in ipairs(parse_results) do
@@ -190,14 +252,17 @@ function diagnostics.terminal_from_parse_results(parse_results)
             col = parse_result.range.from.col,
             end_col = parse_result.range.to.col,
             severity = vim.diagnostic.severity[severity],
-            message = ("%s %s"):format(parse_result.kind, severity:lower()),
-            source = parse_result.source,
+            message = ("%s %s"):format(
+                parse_result.command_spec:kind(),
+                severity:lower()
+            ),
+            source = parse_result.command_spec:name(),
             code = code,
         })
 
         table.insert(terminal_diagnostics, diagnostic)
 
-        :: continue ::
+        ::continue::
     end
 
     return terminal_diagnostics
@@ -211,23 +276,24 @@ function diagnostics.set(buffer, _diagnostics, options)
 end
 
 ---@param _diagnostics vim.Diagnostic[]
----@param options terminal-diagnostics.QuickfixOptions?
+---@param options terminal-diagnostics.ListOptions?
 function diagnostics.setqflist(_diagnostics, options)
     local _options = options or {}
 
-    -- TODO: What to do about multiple paths here?
     local qf_items = vim.diagnostic.toqflist(_diagnostics)
 
-    -- for idx = 1, #qf_items do
-    --     local qf_item = qf_items[idx]
-    --     local diagnostic = _diagnostics[idx]
-    --
-    --     -- Abuse the module name of the quickfix item to signal the
-    --     -- source of the diagnostic
-    --     qf_item.module = diagnostic.source
-    -- end
+    for idx = 1, #qf_items do
+        local qf_item = qf_items[idx]
+        local diagnostic = _diagnostics[idx]
 
-    -- vim.print(vim.inspect(qf_items))
+        -- Abuse the module name of the quickfix item to signal the source of
+        -- the diagnostic
+        if diagnostic.source then
+            local relative_path = vim.fn.fnamemodify(diagnostic.user_data.path, ":.")
+
+            qf_item.module = ("%s (%s)"):format(relative_path, diagnostic.source)
+        end
+    end
 
     -- TODO: Use context to replace existing quickfix?
     vim.fn.setqflist({}, " ", {
@@ -237,32 +303,22 @@ function diagnostics.setqflist(_diagnostics, options)
     })
 
     if _options.open then
-        vim.cmd.copen()
+        local win_id = vim.api.nvim_get_current_win()
+        diagnostics.copen(win_id)
 
         if _options.focus == false then
-            vim.cmd.wincmd("p")
+            vim.api.nvim_set_current_win(win_id) -- TODO: Necessary?
         end
     end
 end
 
+---@param win_id integer
+function diagnostics.copen(win_id)
+    vim.api.nvim_win_call(win_id, vim.cmd.copen)
+end
+
 ---@param options vim.diagnostic.Opts?
 function diagnostics.setup(options)
-    vim.api.nvim_create_autocmd("BufReadPost", {
-        callback = function(event)
-            -- TODO: Allow users to set when diagnostics are set (e.g. if there is no lsp)
-
-            -- 1. Get absolute filename
-            local filename = vim.api.nvim_buf_get_name(event.buf)
-
-            -- 2. See if there is a cache entry for the filename
-            -- local result = diagnostics_cache:get(filename)
-
-            -- 3. Set diagnostics and remove cache entry
-            -- diagnostics.set(event.buf, result, {})
-            -- diagnostics_cache:remove(filename)
-        end,
-    })
-
     local diagnostic_config = vim.tbl_deep_extend("force", {
         underline = false,
         severity_sort = true,
